bbba4f03bd6dc774af301810fee5518b70b41a4a
kiry5273 <kirstenyyw@gmail.com>
2024-05-31 18:09:36 -0700

    add_file

---
+++ src/build/champ_base/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ubuntu/ros2_ws/src/install/champ/share/champ/package.sh"
+. "/home/ubuntu/ros2_ws/src/install/champ_msgs/share/champ_msgs/package.sh"
---
+++ src/build/mini_pupper_dance/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ubuntu/ros2_ws/src/install/mini_pupper_interfaces/share/mini_pupper_interfaces/package.sh"
